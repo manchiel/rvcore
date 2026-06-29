07b8f60f77a46866a7c04fa59860d39c10f16764
Nemanja <marceticcontact@gmail.com>
2026-06-29 02:37:55 +0200

    Implement RV64I instructions except branches

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,10 +1,10 @@
 use crate::bus::Bus;
 use crate::dram::DRAM_BASE;
 
-pub struct CPU{ //pub allows other modules to use cpu structure and methods
+pub struct CPU{             //pub allows other modules to use cpu structure and methods
     pub reg : [u64;32],
     pub pc: u64,
-    pub system_bus: Bus // talking to memory (dram) throw system bus
+    pub system_bus: Bus     // talking to memory (dram) throught system bus
 }
 
 impl CPU{
@@ -26,12 +26,13 @@ impl CPU{
 
         //decode
 
-        //always on that place so we can extract before decoding
+        //always on that place so we can extract before match
         let opcode = instruction & 0x7f;
         let rd = ((instruction >> 7 ) & 0x1f as u32) as usize;
         let rs1 = ((instruction >> 15 ) & 0x1f as u32) as usize;       
         let rs2 = ((instruction >> 20 ) & 0x1f as u32) as usize;
-        let func3 =((instruction >> 12) & 0x7 as u32) as usize;
+        let func3 = ((instruction >> 12) & 0x7 as u32) as usize;
+        let func7 = ((instruction >> 25) & 0x7f as u32) as usize;
 
 
         //execute
@@ -105,16 +106,204 @@ impl CPU{
                     _ => {Err(())},
                 }
             }
-  
-            0x33 => {
-                self.reg[rd] = self.reg[rs1].wrapping_add(self.reg[rs2]);
-                Ok(())
-            }
-            0x13 => {
+
+            0x13 => { // REGISTER - IMMEDIATE ALU ( I type )
                 let imm = ((instruction as i32) >> 20) as i64 as u64;
-                self.reg[rd] = self.reg[rs1].wrapping_add(imm); //wrapping add safer than '+' if aritmetic overflow happens
-                Ok(())
+                let shamt = (instruction >> 20) & 0x3f;
+                let srl_bit = (instruction >> 30) & 1; // i could also use func7 
+                match func3{
+                    0x0 =>{ //addi
+                        self.reg[rd] = self.reg[rs1].wrapping_add(imm);                     //wrapping add safer than '+' if aritmetic overflow happens
+                        Ok(())
+                    },
+                    0x1 =>{ //slli
+                        self.reg[rd] = self.reg[rs1] << shamt;                                      // rd = rs1 << con[0:5]
+                        Ok(())          
+                    },
+                    0x2 =>{ //slti
+                        self.reg[rd] = if (self.reg[rs1] as i64) < (imm as i64) { 1 } else { 0 };   //rd = (rs1 < sext(con)) ? 1 : 0  
+                        Ok(())                           
+                    },
+                    0x3 =>{ //sltiu
+                        self.reg[rd] = if self.reg[rs1] < imm { 1 } else { 0 };                     //rd = (rs1 < sext(con)) ? 1 : 0 
+                        Ok(())                                                              //(imm is sign extended , but < is unsigned operation here)
+                    },
+                    0x4 =>{ //xori
+                        self.reg[rd] = self.reg[rs1] ^ imm;                                         // rd = rs1 ^ sext(con)
+                        Ok(())
+                    },
+                    0x5 =>{ //srai 
+                        if srl_bit==1 {self.reg[rd] = ((self.reg[rs1] as i64) >> shamt) as u64}     //rd = rs1 >> con[0:5]
+                            //srli
+                        else {self.reg[rd] = self.reg[rs1] >> shamt};                               //rd = rs1 >> con[0:5]
+                        Ok(())
+                    },
+                    0x6 =>{ //ori
+                        self.reg[rd] = self.reg[rs1] | imm;                                         //rd = rs1 | sext(con)
+                        Ok(())
+                    },
+                    0x7 =>{ //andi
+                        self.reg[rd] = self.reg[rs1] & imm;                                         //rd = rs1 & sext(con)
+                        Ok(())
+                    },
+                    _ =>{
+                        println!("Invalid register-immediate AL operation");
+                        Err(())
+                    },
+
+                }
+                
             }
+            0x33 => { //REGISTER - REGISTER ALU ( R type )
+                let shamt = (self.reg[rs2] & 0x3f) as u64; // shamt = rs2[0:5]
+                let srl_bit = (instruction >> 30) & 1; // telling is it arithmetic or logic, i could also use func7 
+                match func3{
+                    0x0 =>{ 
+                        match func7{
+                            0x00=>{ // add 
+                                self.reg[rd] = self.reg[rs1].wrapping_add(self.reg[rs2]);   //rd = rs1 + rs2
+                                Ok(())
+                            },
+                            0x20 =>{ //sub
+                                self.reg[rd] = self.reg[rs1].wrapping_sub(self.reg[rs2]);   //rd = rs1 - rs2
+                                Ok(())
+                            },
+                            _ => {
+                                println!("Invalid register-register add/mul/sub operation");
+                                Err(())
+                            },
+                        }
+                    },
+                    0x1 =>{ //sll
+                        self.reg[rd] = self.reg[rs1] << shamt;     //rd = rs1 << rs2[0:5]
+                        Ok(())      
+                    },
+                    0x2 =>{ //slt
+                        self.reg[rd] = if ( self.reg[rs1] as i64 ) < ( self.reg[rs2] as i64) {1} else {0};  //rd = (rs1 < rs2) ? 1 : 0
+                        Ok(())
+                    },
+                    0x3 =>{ //sltu
+                        self.reg[rd] = if self.reg[rs1] < self.reg[rs2] {1} else {0};   //rd = (rs1 < rs2) ? 1 : 0
+                        Ok(())                                                            
+                    },
+                    0x4 =>{ //xor
+                        self.reg[rd] = self.reg[rs1] ^ self.reg[rs2]; //rd = rs1 ^ rs2
+                        Ok(())
+                    },
+                    0x5 =>{ 
+                        //sra
+                        if srl_bit==1 {self.reg[rd] = ((self.reg[rs1] as i64) >> shamt) as u64}     //rd = rs1 >> rs2[0:5]
+                        //srl
+                        else {self.reg[rd] = self.reg[rs1] >> shamt};                               // rd = rs1 >> rs2[0:5]    
+                        Ok(())                                
+                    },
+                    0x6 =>{ //or
+                        self.reg[rd] = self.reg[rs1] | self.reg[rs2];       //rd = rs1 | rs2
+                        Ok(())
+                    },
+                    0x7 =>{ //and
+                        self.reg[rd] = self.reg[rs1] & self.reg[rs2];   //rd = rs1 & rs2
+                        Ok(())
+                    },
+                    _ =>{
+                        println!("Invalid register-register AL operation");
+                        Err(())
+                    },
+
+                }
+            }
+            0x1B =>{ // W REGISTER - IMMEDIATE ALU ( I type ) 32-bit
+                let imm = ((instruction as i32) >> 20) as i64 as u64;
+                let shamt = (instruction >> 20) & 0x1f;
+                let srl_bit = (instruction >> 30) & 1; 
+                match func3{
+                    0x0 =>{ //addiw
+                        self.reg[rd] = self.reg[rs1].wrapping_add(imm) as i32 as i64 as u64;       //rd = sext((rs1 + sext(con))[0:31])              
+                        Ok(())
+                    },
+                    0x1 =>{ //slliw
+                        self.reg[rd] = ((self.reg[rs1] as i32) << shamt) as i64 as u64;         //rd = sext((rs1 << con[0:4])[0:31])                              
+                        Ok(())          
+                    },
+                    0x5 =>{ //sraiw 
+                        if srl_bit==1 {self.reg[rd] = ((self.reg[rs1] as i32) >> shamt) as i64 as u64}     //sext(rd[0:31] = rs1 >> con[0:4])
+                            //srliw
+                        else { self.reg[rd] = ((self.reg[rs1] as u32) >> shamt) as i32 as i64 as u64; }                //rd = sext(rs1[0:31] >> con[0:4])               
+                        Ok(())
+                    },
+                    _ =>{
+                        println!("Invalid W register-immediate AL 32-bit operation");
+                        Err(())
+                    },
+                }
+            }
+            0x3B => { // W REGISTER - REGISTER ALU ( R type ) 32-bit
+                let shamt = (self.reg[rs2] & 0x1f) as u32; //rs2[0:4]
+                let srl_bit = (instruction >> 30) & 1;
+                match func3{
+                    0x0 =>{
+                        match func7{
+                            0x00=>{ // addw 
+                                self.reg[rd] =  self.reg[rs1].wrapping_add(self.reg[rs2]) as i32 as i64 as u64; //rd = sext((rs1 + rs2)[0:31])
+                                Ok(())
+                            },
+                            0x20 =>{ //subw
+                                self.reg[rd] = self.reg[rs1].wrapping_sub(self.reg[rs2]) as i32 as i64 as u64; //rd = sext((rs1 - rs2)[0:31])
+                                Ok(())
+                            },
+                            _ =>{
+                                println!("Invalid W register-register add/sub 32-bit operation");
+                                Err(())
+                            },
+                        }
+                    },
+                    0x1 =>{ //sllw
+                        self.reg[rd] = ((self.reg[rs1] as i32) << shamt) as i64 as u64;      //rd = sext((rs1 << rs2[0:4])[0:31])
+                        Ok(()) 
+                    },
+                    0x5 =>{ 
+                        //sraw
+                        if srl_bit==1 { self.reg[rd] = ((self.reg[rs1] as i32) >> shamt) as i64 as u64; }     //rd = rs1 >> rs2[0:5]
+                        //srlw
+                        else { self.reg[rd] = ((self.reg[rs1] as u32) >> shamt) as i32 as i64 as u64; }                  // rd = rs1 >> rs2[0:5]    
+                        Ok(())
+                    },
+                    _ =>{
+                        println!("Invalid W register-register 32-bit operation");
+                        Err(())
+                    },
+                }
+            },
+            0x37 =>{ //lui ( U type )
+                self.reg[rd] = (instruction & 0xfffff000) as i32 as i64 as u64; //rd = sext(con[31:12 << 12)
+                Ok(())
+            },
+            0x17 =>{ //auipc ( U type )
+                let imm = (instruction & 0xfffff000) as i32 as i64 as u64;
+                self.reg[rd] = self.pc.wrapping_add(imm).wrapping_sub(4); //rd = pc + sext(con[31:12] << 12)
+                //-4 because in main I incremented pc to next instruction before execution of current one
+                Ok(())
+            },
+            0x6f =>{ //jal ( J type )
+                self.reg[rd] = self.pc; // already on pc+4
+                let imm = (((instruction & 0x80000000) as i32 as i64 >> 11) as u64) //extracting immediate from J type instruction
+                        | (instruction & 0xff000) as u64                       
+                        | ((instruction >> 9) & 0x800) as u64 
+                        | ((instruction >> 20) & 0x7fe) as u64; 
+
+                self.pc = self.pc.wrapping_add(imm).wrapping_sub(4);    //rd = pc + 4; pc += sext(con)
+                Ok(())
+
+            },
+            0x67 =>{ //jalr ( I type )
+                let old_pc = self.pc;
+                let imm = ((((instruction & 0xfff00000) as i32) as i64) >> 20) as u64;
+                self.pc = (self.reg[rs1].wrapping_add(imm)) & !1;      //rd = pc + 4; pc = (rs1 + sext(offset)) & ~1 
+                // & !1 clears the lowest bit to force an even (aligned) address
+                self.reg[rd] = old_pc; //link
+                Ok(())
+            },
+        
             _=> {println!("Not supported command detected"); Err(())},
         }
         
